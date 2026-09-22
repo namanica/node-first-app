2bd3dcfe2a716ee6b276eac277ebd9a7a7ef07e3
namanica <veronichkapilipna@gmail.com>
2026-09-22 15:24:06 +0300

    feat: add HTML response with test content

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const http = require("http");
+const testHtml = require("./constants/test-html");
 
 const requestListener = (req, res) => {
-  console.log("REQUEST:", req);
-  console.log("RESPONSE:", res);
+  // console.log("REQUEST:", req);
+  // console.log("RESPONSE:", res);
+  console.log(req.method, req.url);
+
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
+  res.write(testHtml);
+  res.end();
 };
 
 const server = http.createServer(requestListener);
---
+++ constants/test-html.js
@@ -0,0 +1,13 @@
+const testHtml = `
+<!DOCTYPE html>
+<html>
+<head><title>Header Test</title></head>
+<body>
+  <h1 style="color: tomato;">Hello from Node!</h1>
+  <p>This is <b>bold</b> and <i>italic</i>.</p>
+  <p>Привіт, світ! 👋</p>
+</body>
+</html>
+`;
+
+module.exports = testHtml;
